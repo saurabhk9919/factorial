ebbcf7b52b1836119f3c48d192624bb36bdece50
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2024-12-03 23:14:20 +0530

    Add files via upload

---
+++ pythonpro/pattern22.py
@@ -0,0 +1,10 @@
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if j == 0 or (i == 0 or i == rows // 2) and j < cols - 1 or (j == cols - 1 and i != 0 and i < rows // 2) or (i == j and i >= rows // 2):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
