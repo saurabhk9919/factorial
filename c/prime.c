9da378aa518405a3281f79934814eb0b3ec2287c
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2024-12-25 23:08:49 +0530

    Add files via upload

---
+++ c/prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int start, end;
+
+    printf("Enter the range (start and end): ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
