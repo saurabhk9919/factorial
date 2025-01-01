00ba477ab551b3334dfd282003aa0aa1dd4d5ebf
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-01-01 23:06:23 +0530

    Update array8.c

--- array8.c
+++ array8.c
@@ -5,7 +5,7 @@ for(int i=0;i<=6;i++){
     printf("enter the  numbers:");//greatest number in the  given array
     scanf("%d",&a[i]);
 }
-int max=a[0];
+int max = a[0];
 for(int i=1;i<=6;i++){
     if(max>a[i]){//greatest print karane k liye ineqality ka sign change krdo
         max=a[i];
