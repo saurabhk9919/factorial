4389aa2aa750967e135a60588fd22e88832ec4fe
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-01-07 00:10:41 +0530

    Update 123.c

--- c/123.c
+++ c/123.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d",&year);
      if ((year%4==0)&&(year%100!=100||year%400==0)){
         printf("the year is leap");
-     }
+      }
      else
      {
         printf("not a leap year");
