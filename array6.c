5ee7107f5951e7ddd6c7b9df30ac4d601bfdfa0a
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-01-20 23:07:50 +0530

    Update array6.c

--- array6.c
+++ array6.c
@@ -6,8 +6,7 @@ int main(){
         scanf("%d",a[i]); 
     }
               int sum=0;
-            for(int i=0;i<=4;i++){//isme user se input lena hai pr ho nhi                                   //
-                    //hai
+            for(int i=0;i<=4;i++)
       
               sum=sum + a[i];
             printf("%d",sum);
