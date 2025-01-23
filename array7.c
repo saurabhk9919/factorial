ef9b2c16e5b843b633612bcd73d1f3cf442704e0
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-01-23 23:07:59 +0530

    Update array7.c

--- array7.c
+++ array7.c
@@ -6,7 +6,6 @@ for(int i=0;i<=3;i++){
     printf("enter the element %d:",i+1);
     scanf("%d",&a[i]);
 }
-
 int product=1;
 for(int i=0;i<=3;i++){//product of the given elements of the array by the user
     product=product*a[i];
