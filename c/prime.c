95202ba343e34a79535ab0cf127b3e4a0d18812e
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-02-13 23:36:56 +0530

    Update prime.c

--- c/prime.c
+++ c/prime.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-int isPrime(int num) {
+int isPrime(int num) {
     if (num <= 1) return 0;
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return 0;
     }
     return 1;
 }
-int main() {
+int main() {
     int start, end;
-printf("Enter the range (start and end): ");
+printf("Enter the range (start and end): ");
     scanf("%d %d", &start, &end);
-
     printf("Prime numbers between %d and %d are:\n", start, end);
-
     for (int i = start; i <= end; i++) {
         if (isPrime(i)) {
             printf("%d ", i);
         }
     }
-
     printf("\n");
     return 0;
 }
