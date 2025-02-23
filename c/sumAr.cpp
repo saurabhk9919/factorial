6d30fa44351a7d1c603dacc5bd046a61746871d9
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-02-23 23:17:18 +0530

    Update sumAr.cpp

--- c/sumAr.cpp
+++ c/sumAr.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
 using namespace std;
-int main() {
+int main() {
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
     int arr[n];
     cout << "Enter the elements of the array: ";
-    for(int i = 0; i < n; i++) 
-{
-        cin >> arr[i];
+    for(int i = 0; i < n; i++) {
+    cin >> arr[i];
     }
     int sum = 0;
-    for(int i = 0; i < n; i++)
- {
+    for(int i = 0; i < n; i++){
         sum += arr[i];
     }
     cout << "The sum of the array elements is: " << sum << endl;
