df1aebdd29ad4af246c58e5706d6fd28c3f00c64
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-03-04 23:41:28 +0530

    Update 123.c

--- c/123.c
+++ c/123.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main ()
 {
-    int year;
-    printf("enter the year");
-    scanf("%d",&year);
+  int year;
+  printf("enter the year");
+  scanf("%d",&year);
      if ((year%4==0) && (year%100! = 100||year % 400==0)){
         printf("It is a leap year");
       }
