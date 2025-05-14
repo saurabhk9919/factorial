824b416b944edc0dce2303091426fcda18cf6184
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-05-14 21:56:07 +0530

    Update arlinsrch.java

--- arlinsrch.java
+++ arlinsrch.java
@@ -2,14 +2,13 @@ import java.util.*;
 public class arlinsrch{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter the size of the arrray elemnt:");
-        int size=sc.nextInt (); 
-        int flag=0;
+   System.out.print("Enter the size of the arrray elemnt:");
+   int size=sc.nextInt (); 
+int flag=0;
         int position=-1;
         int a[]=new int[size];
         System.out.println("Enter the key to search:");
         int key=sc.nextInt();
-       // int flag=sc.nextInt();
         System.out.println("Enter the array elements");
         for(int i=0;i<size;i++)
             a[i]=sc.nextInt();
@@ -20,8 +19,8 @@ public class arlinsrch{
                 break;
           }
         }
-                if(flag==1){
-                System.out.print("The elemnt is " +key+ " at "+(position+1) );
+      if(flag==1){
+    System.out.print("The elemnt is " +key+ " at "+(position+1) );
             }
             else{
                 System.out.print("ELmemt not found");
