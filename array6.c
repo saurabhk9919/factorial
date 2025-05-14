7eb1b54e5d9e9fd41b452289e7ebaf691f68a727
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-05-14 21:51:49 +0530

    Update array6.c

--- array6.c
+++ array6.c
@@ -3,12 +3,12 @@ int main(){
     int a[5];
     for(int i=0;i<=4;i++){
         printf("enter the element %d;",i+1);
-     scanf("%d",a[i]); 
+    scanf("%d",a[i]); 
     }
-    int sum=0;
-          for(int i=0;i<=4;i++)
-             sum=sum + a[i];
-           printf("%d",sum);
+int sum=0;
+    for(int i=0;i<=4;i++)
+     sum=sum + a[i];
+ printf("%d",sum);
      }
         return 0;
     }
